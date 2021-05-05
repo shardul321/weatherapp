06118198038cf6b4338ec6927f3cde2e156b8dc7
shardul <surmashardul@gmail.com>
2021-05-05 11:44:07 +0530

    Updates

---
+++ precache-manifest.8f6531912261e1910967a982db88db25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5d39adbc8d27012a73297c5e4096a97",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "7144fcbd9da4e9e3e778",
+    "url": "/weatherapp/static/css/main.ac503996.chunk.css"
+  },
+  {
+    "revision": "3696c363641e190a7541",
+    "url": "/weatherapp/static/js/2.d54fc02e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherapp/static/js/2.d54fc02e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7144fcbd9da4e9e3e778",
+    "url": "/weatherapp/static/js/main.8cb92da3.chunk.js"
+  },
+  {
+    "revision": "77278465cd1cff472f88",
+    "url": "/weatherapp/static/js/runtime-main.1df6069b.js"
+  }
+]);
