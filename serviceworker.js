2b8e3a5b47ad8ea8d444a27ced7cd4a241782736
shardul <surmashardul@gmail.com>
2021-05-05 12:45:48 +0530

    Updates

--- serviceworker.js
+++ serviceworker.js
@@ -1,74 +1,46 @@
-// const CACHE_NAME = "version-1";
+const CACHE_NAME = "version-1";
 const urlsToCache = [ 'index.html', 'offline.html' ];
 
-// const self = this;
+const self = this;
 
-// // Install SW
-// self.addEventListener('install', (event) => {
-//     event.waitUntil(
-//         caches.open(CACHE_NAME)
-//             .then((cache) => {
-//                 console.log('Opened cache');
-//                 return cache.addAll(urlsToCache);
-//             })
-//     )
-// });
-
-// // Listen for requests
-// self.addEventListener('fetch', (event) => {
-//     event.respondWith(
-//         caches.match(event.request)
-//             .then(() => {
-//                 return fetch(event.request) 
-//                     .catch(() => caches.match('offline.html'))
-//             })
-//     )
-// });
-
-// // Activate the SW
-// self.addEventListener('activate', (event) => {
-//     const cacheWhitelist = [];
-//     cacheWhitelist.push(CACHE_NAME);
-
-//     event.waitUntil(
-//         caches.keys().then((cacheNames) => Promise.all(
-//             cacheNames.map((cacheName) => {
-//                 if(!cacheWhitelist.includes(cacheName)) {
-//                     return caches.delete(cacheName);
-//                 }
-//             })
-//         ))
-            
-//     )
-// });
-  
-const cacheData = "appV1";
-this.addEventListener("install", (event) => {
+// Install SW
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(cacheData).then((cache) => {
-            cache.addAll(urlsToCache)
-        })
-    )
-})
-this.addEventListener("fetch", (event) => {
-
-
-    if (!navigator.onLine) {
-        if (event.request.url === "http://localhost:3000/static/js/main.chunk.js") {
-            event.waitUntil(
-                this.registration.showNotification("Internet", {
-                    body: "internet not working",
-                })
-            )
-        }
-        event.respondWith(
-            caches.match(event.request).then((resp) => {
-                if (resp) {
-                    return resp
-                }
-                let requestUrl = event.request.clone();
-                fetch(requestUrl)
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                 cache.addAll(urlsToCache);
             })
-        )
-    }
-}) 
+    )
+});
+
+// Listen for requests
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+         caches.match(event.request).then((resp) => {
+          if (resp) {
+               return resp
+          }
+           let requestUrl = event.request.clone();
+          fetch(requestUrl)
+          .catch(() => caches.match('offline.html'))
+        })
+                )
+});
+
+// Activate the SW
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhitelist.includes(cacheName)) {
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+            
+    )
+});
+  
